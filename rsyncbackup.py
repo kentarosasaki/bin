7ab31d6e73da19550759bd43a5693499695a3209
Kentaro Sasaki <mathomd@gmail.com>
2012-06-06 00:49:48 +0900

    rsyncbackup.py

--- rsyncbackup.py
+++ rsyncbackup.py
@@ -1,11 +1,12 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-import commands
 import logging
 import os
 import os.path
 import re
+import shlex
+import subprocess
 import time
 from datetime import date
 
@@ -24,8 +25,10 @@ class RsyncBackup:
         Returns true if successful, false if an error occurs.
         """
         cmnd = "%s '%s' '%s' -acvzut" % (self.rsync, source, destination)
-        self.log.debug ("Running command: %s" % cmnd)
-        result = commands.getstatusoutput(cmnd)
+        args = shlex.split(cmnd)
+        self.log.debug ("Running command: %s" % args)
+        backuputil = BackupUtils()
+        result = backuputil.get_status_output(args)
         if (result[0] == 0):
             self.log.info("Rsync backup successful.")
             self.log.debug("Rsync output: %s" % result[1])
@@ -35,24 +38,14 @@ class RsyncBackup:
         return 1
 
     def trim_archives(self, removedir, retention=90):
-        """ Delete old archives - WARNING: This deletes files, be careful with it.
+        """ Delete old archives - This deletes files, be careful with it.
         removeDir     - The directory which is deleted.
-        retention     - (Optional) Delete older than this days, defaults to 7.
+        retention     - (Optional) Delete older than this days, defaults to 90.
         """
         self.log.info("Starting deletion.")
         limit_timestamp = time.time() - 86400 * retention
-        def _os_walk_cache(dir):
-            cache = {}
-            if dir in cache:
-                for x in cache[dir]:
-                    yield x
-            else:
-                cache[dir] = []
-                for x in os.walk(dir):
-                    cache[dir].append(x)
-                    yield x
-            raise StopIteration()
-        for root, dirs, files in _os_walk_cache(removedir):
+        backuputil = BackupUtils()
+        for root, dirs, files in backuputil.os_walk_cache_util(removedir):
             # If file is all old, turn delflag True
             for file in files:
                 filepath = os.path.join(root, file)
@@ -60,7 +53,8 @@ class RsyncBackup:
                 if filemtime < limit_timestamp: # Old file
                     try:
                         os.unlink(filepath)
-                        self.log.info("File deletion successful.")
+                        self.log.info("File deletion successful in %s" %
+                                      filepath)
                     except OSError, e:
                         pass
             # Delete blank directories
@@ -68,6 +62,31 @@ class RsyncBackup:
                 dirpath = os.path.join(root, dir)
                 try:
                     os.rmdir(dirpath)
-                    self.log.info("Blank Directory deletion successful.")
+                    self.log.info("Blank Directory deletion successful in %s" %
+                                  dirpath)
                 except OSError, e:
                     pass
+
+class BackupUtils:
+    def get_status_output(self, command):
+        # Return (status, output) of executing command in a shell.
+        pipe = subprocess.Popen(command, universal_newlines=True,
+                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = str.join("", pipe.stdout.readlines())
+        status = pipe.wait()
+        if status is None:
+            status = 0
+        return status, output
+
+    def os_walk_cache_util(self, directory):
+        cache = {}
+        if directory in cache:
+            for x in cache[directory]:
+                yield x
+        else:
+            cache[directory] = []
+            for x in os.walk(directory):
+                cache[directory].append(x)
+                yield x
+        raise StopIteration()
+
