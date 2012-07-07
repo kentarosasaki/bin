aad7343f1ad08d4027c883e50f27e07d67c97267
Kentaro Sasaki <mathomd@gmail.com>
2012-07-07 19:46:55 +0900

    collect.py

--- collect.py
+++ collect.py
@@ -1,54 +1,63 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
 import csv
+import logging
+import logging.handlers
 import os
 import socket
 import sys
 
-from lib import log
 from lib import sync
-from lib import trimming
 
 
-def csv_to_list(csvname):
-    return [row for row in csvname if os.path.isdir(row[0]) and
-                                      os.path.isdir(row[1])]
+def logging_config(logging_file):
+    # Logging to a file done here.
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+                '%(asctime)s %(levelname)s %(name)s %(message)s')
+    handler = logging.handlers.TimedRotatingFileHandler(
+              filename=logging_file,
+              when='D',
+              backupCount=7)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+def csv_to_list(csv_name):
+    return [row for row in csv_name if os.path.isdir(row[0]) and
+                                       os.path.isdir(row[1])]
 
 
 def main():
 
     # Set init file path.
-    initfilepath = os.path.dirname(os.path.abspath(__file__))
+    init_file_path = os.path.dirname(os.path.abspath(__file__))
 
     # Create a resource file object.
     if not len(sys.argv) == 2:
-        sys.exit('Usage: %s mappingfile' % sys.argv[0])
-
-    mappingfile = os.path.join(''.join((initfilepath, os.sep, "config")),
-                               sys.argv[1])
-
-    if not os.path.exists(mappingfile):
-        sys.exit('ERROR: Mapping File %s was not found!' % mappingfile)
-
-    csvfile = open(mappingfile, 'r')
-    csvloop = csv.reader(csvfile)
+        sys.exit('Usage: %s CSVfile' % sys.argv[0])
+    mapping_file = os.path.join(''.join((init_file_path, os.sep, "config")),
+                                sys.argv[1])
+    if not os.path.exists(mapping_file):
+        sys.exit('ERROR: CSV File %s was not found!' % mapping_file)
+    csv_file = open(mapping_file, 'r')
+    csv_loop = csv.reader(csv_file)
 
     # Create a logging file.
     host = socket.gethostname()
-    loggingfilename = ''.join(("backup_", host, ".log"))
-    loggingfile = os.path.join(''.join((initfilepath, os.sep, "log")),
-                               loggingfilename)
-    log.logging_config(loggingfile)
+    logging_file_name = ''.join(("backup_", host, ".log"))
+    logging_file = os.path.join(''.join((init_file_path, os.sep, "log")),
+                                logging_file_name)
+    logging_config(logging_file)
 
     # Create objects.
     backup = sync.Sync()
-    delete = trimming.Trimming()
-    [(backup.sync(path[0], path[1]), delete.trimming(path[1]))
-      for path in csv_to_list(csvloop)]
+    [(backup.run(path[0], path[1])) for path in csv_to_list(csv_loop)]
 
     # Close csv file.
-    csvfile.close()
+    csv_file.close()
 
 
 if __name__ == '__main__':
--- sync.py
+++ sync.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
+#!/usr/local/bin/python
 # -*- coding: utf-8 -*-
+
 
 import logging
 import os
@@ -13,101 +14,106 @@ class Sync(object):
         self.cache = {}
         self.log = logging.getLogger("Sync")
 
-    def os_listdir_cache(self, directory):
+    def os_walk_cache(self, directory):
         if directory in self.cache:
             for x in self.cache[directory]:
                 yield x
         else:
             self.cache[directory] = []
-            for x in os.listdir(directory):
+            for x in os.walk(directory):
                 self.cache[directory].append(x)
                 yield x
         raise StopIteration()
 
-    def listdir(self, path):
-        ans = {}
-        for item in self.os_listdir_cache(path):
-            fullpath = os.path.join(path, item)
-            if os.path.isdir(fullpath):
-                ans[''.join([item, os.sep])] = self.listdir(fullpath)
-            else:
-                ans[item] = ()
-        return ans
+    def search_file(self, directory):
+        return [os.path.join(root, file)
+                for root, dirs, files in self.os_walk_cache(directory)
+                for file in files]
 
-    def subtract(self, include, exclude):
-        ans = {}
-        for key, value in include.items():
-            if isinstance(value,dict):
-                if value != exclude.get(key, None):
-                    ans[key] = self.subtract(value, exclude.get(key,{}))
-            else:
-                if not key in exclude:
-                    ans[key] = tuple(value)
-        return ans
-
-    def merge(self, include, extend):
-        ans = self.subtract(include, {})
-        for key, value in extend.items():
-            if isinstance(value,dict):
-                ans[key] = self.merge(value, include.get(key, {}))
-            else:
-                ans[key] = tuple(value)
-        return ans
+    def cut_base_dir(self, base_dir, path):
+        return [item.replace(base_dir, "") for item in path]
 
     def copy(self, source, destination, index):
-        if not os.path.exists(destination):
-            self.log.debug("Make directory: %s" % destination)
-            os.makedirs(destination)
-        for key, value in index.items():
-            source_item = os.path.join(source, key)
-            destination_item = os.path.join(destination, key)
-            if isinstance(value, dict):
-                self.copy(source_item, destination_item, value)
-            else:
+        """ Copy files.
+        source      - The source directory who's contents should be backed up.
+        destination - The directory that the backup should go into.
+        index       - The index that exist only source.
+        """
+        for index_elem in index:
+            source_item = os.path.join(source, index_elem)
+            destination_item = os.path.join(destination, index_elem)
+            destination_dir = os.path.dirname(destination_item)
+            if not os.path.exists(destination_dir):
+                self.log.debug("Make directory: %s" % destination_dir)
+                os.makedirs(destination_dir)
+            try:
+                self.log.debug("Copy file: %s" % destination_item)
+                shutil.copy2(source_item, destination_item)
+            except Exception as msg:
+                self.log.error("Copy error: %s %s %s"
+                               % (source, destination, str(msg)))
+                pass
+        return 1
+
+    def update(self, source, destination, index):
+        """ Update existing files.
+        source      - The source directory who's contents should be backed up.
+        destination - The directory that the backup should go into.
+        index       - The index that exist both source and destination.
+        """
+        for index_elem in index:
+            source_item = os.path.join(source, index_elem)
+            destination_item = os.path.join(destination, index_elem)
+            source_mtime = os.path.getmtime(source_item)
+            destination_mtime = os.path.getmtime(destination_item)
+            if source_mtime > destination_mtime:
                 try:
-                    self.log.debug("Copy file: %s" % destination_item)
+                    self.log.debug("Update file: %s" % destination_item)
                     shutil.copy2(source_item, destination_item)
                 except Exception as msg:
-                    self.log.error("Copy error: %s %s %s"
+                    self.log.error("Update error: %s %s %s"
                                    % (source, destination, str(msg)))
                     pass
         return 1
 
-    def update(self, source, destination, index):
-        for key, value in index.items():
-            source_item = os.path.join(source, key)
-            destination_item = os.path.join(destination, key)
-            if isinstance(value, dict):
-                self.update(source_item, destination_item, value)
-            else:
-                d = os.path.getmtime(source_item) - os.path.getmtime(
-                                                    destination_item)
-                if int(d) > 0:
-                    try:
-                        self.log.debug("Update file: %s" % destination_item)
-                        shutil.copy2(source_item, destination_item)
-                    except Exception as msg:
-                        self.log.error("Update error: %s %s %s"
-                                       % (source, destination, str(msg)))
-                        pass
+    def trimming(self, path, retention=90):
+        """ Delete old archives - This deletes files, be careful with it.
+        path        - The directory which is deleted.
+        retention   - (Optional) Delete older than this days, defaults to 90.
+        """
+        limit_timestamp = time.time() - 86400 * retention
+        for remove_file in path:
+            file_mtime = os.path.getmtime(remove_file)
+            if limit_timestamp > file_mtime: # Old file
+                try:
+                    os.unlink(remove_file)
+                    self.log.debug("File delete successful in %s" %
+                                   remove_file)
+                except Exception as msg:
+                    self.log.error("Delete error: %s %s"
+                                   % (remove_file, str(msg)))
+                    pass
         return 1
 
-    def sync(self, source, destination):
+    def run(self, source, destination):
         """ Perform a backup.
-        source       - The source directory who's contents should be backed up.
-        destination  - The directory that the backup should go into.
-        Returns true if successful, false if an error occurs.
+        source      - The source directory who's contents should be backed up.
+        destination - The directory that the backup should go into.
         """
         self.log.info("Start backup: %s %s" % (source, destination))
-        src = self.listdir(source)
-        dst = self.listdir(destination)
-        merge = self.merge(src, dst)
-        src_only = self.subtract(src, dst)
-        dst_only = self.subtract(dst, src)
-        del_src = self.subtract(merge, src_only)
-        common = self.subtract(del_src, dst_only)
+        full_src = self.search_file(source)
+        full_dst = self.search_file(destination)
+        src = self.cut_base_dir(source, full_src)
+        dst = self.cut_base_dir(destination, full_dst)
+        set_src = set(src)
+        set_dst = set(dst)
+        # Difference set.
+        src_only = set_src - set_dst
+        # Product set.
+        common = set_src & set_dst
         self.copy(source, destination, src_only)
         self.update(source, destination, common)
+        self.trimming(full_dst)
         return 1
 
 
