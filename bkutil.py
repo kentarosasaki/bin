d72c4873345f77a42a107e41b2a68e018c3d06e6
Kentaro Sasaki <mathomd@gmail.com>
2012-07-08 00:44:23 +0900

    bkutil.py

---
+++ bkutil.py
@@ -0,0 +1,156 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+import logging.handlers
+import os
+import socket
+import shutil
+import time
+
+
+# Set init file path.
+init_file_path = os.path.dirname(os.path.abspath(__file__))
+# Create a logging file.
+host = socket.gethostname()
+logging_file_name = ''.join(("backup_", host, ".log"))
+logging_dir = ''.join((init_file_path, os.sep, "log"))
+logging_file = os.path.join(logging_dir, logging_file_name)
+if not os.path.exists(logging_dir):
+    os.makedirs(logging_dir)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+       '%(asctime)s %(levelname)s %(name)s %(message)s')
+handler = logging.handlers.TimedRotatingFileHandler(
+          filename=logging_file,
+          when='D',
+          backupCount=7)
+handler.setFormatter(formatter)
+log.addHandler(handler)
+
+
+class BackupCollector(object):
+
+    def __init__(self):
+        self.log = logging.getLogger("BackupCollector")
+        self.cache = {}
+
+    def os_walk_cache(self, directory):
+        if directory in self.cache:
+            for x in self.cache[directory]:
+                yield x
+        else:
+            self.cache[directory] = []
+            for x in os.walk(directory):
+                self.cache[directory].append(x)
+                yield x
+        raise StopIteration()
+
+    def search_file(self, directory):
+        # Return the fill path of exsinting files.
+        return [os.path.join(root, item)
+                for root, dirs, items in self.os_walk_cache(directory)
+                for item in items]
+
+    def cut_base_dir(self, base_dir, directory):
+        # Return the path which cut base dir.
+        return [item.replace(base_dir, "") for item in directory]
+
+    def copy(self, source, destination, index):
+        """ Copy files.
+
+        source      - The source directory who's contents should be backed up.
+        destination - The directory that the backup should go into.
+        index       - The index that exist only source.
+
+        """
+        for index_elem in index:
+            source_item = os.path.join(source, index_elem)
+            destination_item = os.path.join(destination, index_elem)
+            destination_dir = os.path.dirname(destination_item)
+            if not os.path.exists(destination_dir):
+                self.log.debug("Make directory: %s" % destination_dir)
+                os.makedirs(destination_dir)
+            try:
+                self.log.debug("Copy file: %s" % destination_item)
+                shutil.copy2(source_item, destination_item)
+            except Exception as msg:
+                self.log.error("Copy error: %s %s %s"
+                               % (source, destination, str(msg)))
+                pass
+
+    def update(self, source, destination, index):
+        """ Update existing files.
+
+        source      - The source directory who's contents should be backed up.
+        destination - The directory that the backup should go into.
+        index       - The index that exist both source and destination.
+
+        """
+        for index_elem in index:
+            source_item = os.path.join(source, index_elem)
+            destination_item = os.path.join(destination, index_elem)
+            source_mtime = os.path.getmtime(source_item)
+            destination_mtime = os.path.getmtime(destination_item)
+            duration = source_mtime - destination_mtime
+            if int(duration) > 0:
+                try:
+                    self.log.debug("Update file: %s" % destination_item)
+                    shutil.copy2(source_item, destination_item)
+                except Exception as msg:
+                    self.log.error("Update error: %s %s %s"
+                                   % (source, destination, str(msg)))
+                    pass
+
+    def trimming(self, directory, retention=90):
+        """ Delete old archives - This deletes files, be careful with it.
+
+        directory   - The directory which is deleted.
+        retention   - (Optional) Delete older than this days, defaults to 90.
+
+        """
+        limit_timestamp = time.time() - 86400 * retention
+        for remove_file in directory:
+            file_mtime = os.path.getmtime(remove_file)
+            duration = limit_timestamp - file_mtime
+            if int(duration) > 0:
+                try:
+                    os.unlink(remove_file)
+                    self.log.debug("File delete successful in %s" %
+                                   remove_file)
+                except Exception as msg:
+                    self.log.error("Delete error: %s %s"
+                                   % (remove_file, str(msg)))
+                    pass
+
+    def run(self, source, destination):
+        """ Perform a backup.
+
+        source      - The source directory who's contents should be backed up.
+        destination - The directory that the backup should go into.
+
+        """
+        self.log.info("Start backup: %s %s" % (source, destination))
+        # Get the full path of directory.
+        full_src = self.search_file(source)
+        full_dst = self.search_file(destination)
+        # Get the path which is already cut the base dir in mapping file.
+        src = self.cut_base_dir(source, full_src)
+        dst = self.cut_base_dir(destination, full_dst)
+        # Create set object.
+        set_src = set(src)
+        set_dst = set(dst)
+        # Get the list which is in only source.
+        src_only = set_src - set_dst
+        # Get the line which is in both source and destination.
+        common = set_src & set_dst
+        # Copy the files which is in only source.
+        self.copy(source, destination, src_only)
+        # Update the files which mtime is updated.
+        self.update(source, destination, common)
+        # Trimming old archives which is older than retention, defaults to 90
+        self.trimming(full_dst, retention=90)
+        self.log.info("Finish backup: %s %s" % (source, destination))
+
+
--- collect.py
+++ collect.py
@@ -2,27 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import csv
-import logging
-import logging.handlers
 import os
-import socket
 import sys
 
-from lib import sync
-
-
-def logging_config(logging_file):
-    # Logging to a file done here.
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(
-                '%(asctime)s %(levelname)s %(name)s %(message)s')
-    handler = logging.handlers.TimedRotatingFileHandler(
-              filename=logging_file,
-              when='D',
-              backupCount=7)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+from lib import bkutil
 
 
 def csv_to_list(csv_name):
@@ -31,10 +14,8 @@ def csv_to_list(csv_name):
 
 
 def main():
-
     # Set init file path.
     init_file_path = os.path.dirname(os.path.abspath(__file__))
-
     # Create a resource file object.
     if not len(sys.argv) == 2:
         sys.exit('Usage: %s CSVfile' % sys.argv[0])
@@ -44,18 +25,10 @@ def main():
         sys.exit('ERROR: CSV File %s was not found!' % mapping_file)
     csv_file = open(mapping_file, 'r')
     csv_loop = csv.reader(csv_file)
-
-    # Create a logging file.
-    host = socket.gethostname()
-    logging_file_name = ''.join(("backup_", host, ".log"))
-    logging_file = os.path.join(''.join((init_file_path, os.sep, "log")),
-                                logging_file_name)
-    logging_config(logging_file)
-
-    # Create objects.
-    backup = sync.Sync()
+    # Create a object.
+    backup = bkutil.BackupCollector()
+    # Perform a backup.
     [(backup.run(path[0], path[1])) for path in csv_to_list(csv_loop)]
-
     # Close csv file.
     csv_file.close()
 
