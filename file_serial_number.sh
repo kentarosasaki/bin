00ea9ef7799761623a0c24c83ac63de3be785028
Kentaro Sasaki <mathomd@gmail.com>
2013-06-08 19:58:25 +0900

    file_serial_number.sh

--- file_serial_number.sh
+++ file_serial_number.sh
@@ -5,11 +5,17 @@
 #
 
 PATH=${1}
-DATE=${2}
+#DATE=${2}
+
+#for file in ${PATH}/*.jpg
+#do
+#  mv "$file" `echo $file | tr ' ' '_' | tr '[A-Z]' '[a-z]'`
+#done
+
 i=1
 for f in ${PATH}/*.jpg
 do
   g=0000$i.jpg
-  /bin/mv $f ${PATH}/${2}_${g:(-9)}
+  /bin/mv $f ${PATH}/${g:(-9)}
   i=$((i+1))
 done
