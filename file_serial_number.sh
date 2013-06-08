3e1dd3c2e7339e39626b2cf1de0676e7d0c08fff
Kentaro Sasaki <mathomd@gmail.com>
2013-06-08 19:05:01 +0900

    file_serial_number.sh

--- file_serial_number.sh
+++ file_serial_number.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+#
+# ex).
+# file_serial_number.sh /Users/${USER}/Documents/pictures/${path_to_jpeg} ${date}
+#
 
+PATH=${1}
+DATE=${2}
 i=1
-for f in *.jpg
+for f in ${PATH}/*.jpg
 do
   g=0000$i.jpg
-  mv $f ${g:(-9)}
+  /bin/mv $f ${PATH}/${2}_${g:(-9)}
   i=$((i+1))
 done
