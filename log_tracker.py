c100147c3059ba2130c1b0d8e332b1c76646e49d
Kentaro Sasaki <mathomd@gmail.com>
2013-12-31 22:15:23 +0900

    log_tracker.py

--- log_tracker.py
+++ log_tracker.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
 
-# http://d.hatena.ne.jp/torazuka/20120114/list
-
 
 import collections
 import datetime
+import json
 import os
 import sys
 import time
 
 
-class Summarize(object):
-  def __init__(self, access_data_list):
-    self.access_data_list = access_data_list
+class Aggregate(object):
 
-  def summarize(self, bucket_name):
-    bucket_size = 0
-    head_count = 0
-    put_count = 0
-    get_count = 0
-    delete_count = 0
-    for d in self.access_data_list:
+  def __init__(self, date, access_log_list):
+    self.date = date
+    self.access_log_list = access_log_list
+
+  def bucket_aggregate(self, bucket_name):
+    """ Aggregate each bucket size, count.
+    bucket_name: Logged bucket name.
+    bucket_size: Total bucket size.
+    head_count: Counting of HEAD method.
+    put_count: Counting of PUT method.
+    get_count: Counting of GET method.
+    delete_count: Counting of DELETE method.
+    """
+    bucket_size = head_count = put_count = get_count = delete_count = 0
+    for d in self.access_log_list:
       if bucket_name == d.bucket_name:
         if d.method == "[HEAD]":
           head_count += 1
@@ -32,12 +37,21 @@ class Summarize(object):
         elif d.method == "[DELETE]":
           delete_count += 1
           bucket_size -= int(d.object_size)
-
-    total_count = head_count + put_count + get_count + delete_count
-    return bucket_name, bucket_size, head_count, put_count, get_count, delete_count, total_count
+    aggr_result = [
+        self.date,
+        bucket_name,
+        bucket_size,
+        head_count,
+        put_count,
+        get_count,
+        delete_count,
+        head_count + put_count + get_count + delete_count,
+    ]
+    return aggr_result
 
 
 def define_named_log(access_list):
+  # Deifine namedtuple fields.
   fields = [
       "method",
       "bucket_name",
@@ -51,35 +65,46 @@ def define_named_log(access_list):
   return [named_log(*a) for a in access_list]
 
 
+def list2dict(values):
+  keys = [
+      "time",
+      "bucket",
+      "size",
+      "head",
+      "put",
+      "get",
+      "delete",
+      "total",
+  ]
+  return [dict(zip(keys, v)) for v in values]
+
+
 def main():
+  # Define log timestamp
   log_timestamp = (os.stat(sys.argv[1])).st_mtime
   now = datetime.datetime.now()
   d = now - datetime.timedelta(hours = 1)
   one_hour_ago = time.mktime(d.timetuple())
-  if one_hour_ago < log_timestamp:
+
+  # Open log when timestamp is within 1 hour.
+  if log_timestamp > one_hour_ago:
     with open(sys.argv[1], "r") as f:
-      access_list = [line[:-1].split("\t") for line in f]
+      access_list = [ln[:-1].split("\t") for ln in f]
   else:
-    sys.exit(1)
+    sys.exit("No log within 1 hour.")
 
-  access_data_list = define_named_log(access_list)
-  bucket_sum = Summarize(access_data_list)
-  bucket_list = set([data.bucket_name for data in access_data_list])
-  result = map(bucket_sum.summarize, bucket_list)
+  # Define named log list.
+  access_log_list = define_named_log(access_list)
 
-  print("Get Time, Bucket Name, Bucket Size(Byte), HEAD Count, PUT Count, GET Count, DELETE Count, Total Count")
-  for res in result:
-    print("%s, %s, %d, %d, %d, %d, %d, %d" %
-        (now, res[0], res[1], res[2], res[3], res[4], res[5], res[6]))
-#    print("Get Time : %s" % now)
-#    print("Bucket Name : %s" % res[0])
-#    print("Bucket Size : %d(byte)" % res[1])
-#    print("HEAD Count  : %d" % res[2])
-#    print("PUT Count   : %d" % res[3])
-#    print("GET Count   : %d" % res[4])
-#    print("DELETE Count: %d" % res[5])
-#    print("Total Count : %d" % res[6])
-#    print("")
+  # Generate Aggregate Class.
+  aggr = Aggregate(now.strftime("%Y%m%d%H%M%S"), access_log_list)
+
+  # Generate list which has set by bucket name.
+  bucket_list = set([l.bucket_name for l in access_log_list])
+  aggr_res = map(aggr.bucket_aggregate, bucket_list)
+
+  with open("data.json", "w") as fp:
+    json.dump(list2dict(aggr_res), fp, sort_keys=True)
 
 
 if __name__ == '__main__':
