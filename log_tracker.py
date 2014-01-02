11a74ee9ad4d399efd92a6cdfb2edd2a9ead13be
Kentaro Sasaki <mathomd@gmail.com>
2014-01-02 10:35:54 +0900

    log_tracker.py

--- log_tracker.py
+++ log_tracker.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding=utf-8
 
 
 import collections
@@ -16,38 +17,44 @@ class Aggregate(object):
     self.access_log_list = access_log_list
 
   def bucket_aggregate(self, bucket_name):
-    """ Aggregate each bucket size, count.
-    bucket_name: Logged bucket name.
-    bucket_size: Total bucket size.
-    head_count: Counting of HEAD method.
-    put_count: Counting of PUT method.
-    get_count: Counting of GET method.
-    delete_count: Counting of DELETE method.
+    """ Aggregate each bucket size and method count.
+
+    time: Aggregated time.
+    bucket: Logged bucket name.
+    size: Total bucket size.
+    head: Counting of HEAD method.
+    put: Counting of PUT method.
+    get: Counting of GET method.
+    delete: Counting of DELETE method.
+    total: Counting of all method.
     """
-    bucket_size = head_count = put_count = get_count = delete_count = 0
+    aggr = {
+        "time":self.date,
+        "bucket":bucket_name,
+        "size":0,
+        "head":0,
+        "put":0,
+        "get":0,
+        "delete":0,
+        "total":0,
+    }
     for d in self.access_log_list:
-      if bucket_name == d.bucket_name:
+      if aggr["bucket"] == d.bucket_name:
         if d.method == "[HEAD]":
-          head_count += 1
+          aggr["head"] += 1
+          aggr["total"] += 1
         elif d.method == "[PUT]":
-          put_count += 1
-          bucket_size += int(d.object_size)
+          aggr["put"] += 1
+          aggr["total"] += 1
+          aggr["size"] += int(d.object_size)
         elif d.method == "[GET]":
-          get_count += 1
+          aggr["get"] += 1
+          aggr["total"] += 1
         elif d.method == "[DELETE]":
-          delete_count += 1
-          bucket_size -= int(d.object_size)
-    aggr_result = [
-        self.date,
-        bucket_name,
-        bucket_size,
-        head_count,
-        put_count,
-        get_count,
-        delete_count,
-        head_count + put_count + get_count + delete_count,
-    ]
-    return aggr_result
+          aggr["delete"] += 1
+          aggr["total"] += 1
+          aggr["size"] -= int(d.object_size)
+    return aggr
 
 
 def define_named_log(access_list):
@@ -65,45 +72,45 @@ def define_named_log(access_list):
   return [named_log(*a) for a in access_list]
 
 
-def list2dict(values):
-  keys = [
-      "time",
-      "bucket",
-      "size",
-      "head",
-      "put",
-      "get",
-      "delete",
-      "total",
-  ]
-  return [dict(zip(keys, v)) for v in values]
+def gen_json(aggr_res, modified_now):
+  json_with_time = "".join(("data.json.",str(modified_now)))
+  sym_json = "data.json"
+  with open(json_with_time, "w") as output_log:
+    json.dump(aggr_res, output_log, sort_keys=True)
+  if os.path.islink(sym_json):
+    os.unlink(sym_json)
+    os.symlink(json_with_time, sym_json)
+  else:
+    os.symlink(json_with_time, sym_json)
 
 
 def main():
   # Define log timestamp
   log_timestamp = (os.stat(sys.argv[1])).st_mtime
   now = datetime.datetime.now()
-  an_hour_ago = time.mktime((now - datetime.timedelta(hours = 1)).timetuple())
+  modified_now = now.strftime("%Y%m%d%H%M%S")
+  subtracted_time = time.mktime(
+      (now - datetime.timedelta(hours = 1)).timetuple())
 
-  # Open log when timestamp is within 1 hour.
-  if log_timestamp > an_hour_ago:
-    with open(sys.argv[1], "r") as f:
-      access_list = [ln[:-1].split("\t") for ln in f]
+  # Open log when timestamp is within time subtraction.
+  if log_timestamp > subtracted_time:
+    with open(sys.argv[1], "r") as input_log:
+      access_list = [f[:-1].split("\t") for f in input_log]
   else:
     sys.exit("No log within 1 hour.")
 
-  # Define named log list.
+  # Define named log tuple.
   access_log_list = define_named_log(access_list)
 
   # Generate Aggregate Class.
-  aggr = Aggregate(now.strftime("%Y%m%d%H%M%S"), access_log_list)
+  aggregate = Aggregate(modified_now, access_log_list)
 
   # Generate list which has set by bucket name.
   bucket_list = set([l.bucket_name for l in access_log_list])
-  aggr_res = [aggr.bucket_aggregate(m) for m in bucket_list]
+  aggr_res = [aggregate.bucket_aggregate(m) for m in bucket_list]
 
-  with open("data.json", "w") as fp:
-    json.dump(list2dict(aggr_res), fp, sort_keys=True)
+  # Generage json output.
+  gen_json(aggr_res, modified_now)
 
 
 if __name__ == '__main__':
